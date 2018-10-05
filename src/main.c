7742f47dabfbf6e4b9f1a55ca2e83ad8b5d04d6c
Gyusik Shin <gyusik.shin@atto-research.com>
2018-10-05 14:48:50 +0900

    [DEV] block histogram

--- src/main.c
+++ src/main.c
@@ -9,10 +9,12 @@
 
 #include <setjmp.h>
 
-int32_t histogram[256];
+#define NUM_SAMPLE 10
+
+int32_t histogram[NUM_SAMPLE][3][256];
 
 void
-put_scanline_someplace (JSAMPLE* ba, int row_stride)
+put_scanline_someplace (JSAMPLE* ba, int row_stride, int sample_id)
 {
     static int height;
     int i;
@@ -26,7 +28,9 @@ put_scanline_someplace (JSAMPLE* ba, int row_stride)
         printf("%03d) ", ba[i+2]);
         */
 
-        histogram[ba[i]]++;
+        histogram[sample_id][0][ba[i]]++;
+        histogram[sample_id][1][ba[i+1]]++;
+        histogram[sample_id][2][ba[i+2]]++;
     }
     //printf("\n");
 #endif
@@ -63,14 +67,30 @@ int read_jpeg(const char *filename)
     buffer = (*cinfo.mem->alloc_sarray)
         ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
 
-    while (cinfo.output_scanline < cinfo.output_height) {
+    for (int i = 0; i < NUM_SAMPLE; i++)
+    {
+        while (cinfo.output_scanline < cinfo.output_height &&
+               cinfo.output_scanline < (cinfo.output_height / NUM_SAMPLE) * (i + 1))
+        {
+            /* jpeg_read_scanlines expects an array of pointers to scanlines.
+             * Here the array is only one element long, but you could ask for
+             * more than one scanline at a time if that's more convenient.
+             */
+            (void) jpeg_read_scanlines(&cinfo, buffer, 1);
+            /* Assume put_scanline_someplace wants a pointer and sample count. */
+            put_scanline_someplace(buffer[0], row_stride, i);
+        }
+    }
+
+    while (cinfo.output_scanline < cinfo.output_height)
+    {
         /* jpeg_read_scanlines expects an array of pointers to scanlines.
          * Here the array is only one element long, but you could ask for
          * more than one scanline at a time if that's more convenient.
          */
         (void) jpeg_read_scanlines(&cinfo, buffer, 1);
         /* Assume put_scanline_someplace wants a pointer and sample count. */
-        put_scanline_someplace(buffer[0], row_stride);
+        put_scanline_someplace(buffer[0], row_stride, NUM_SAMPLE - 1);
     }
 
     (void) jpeg_finish_decompress(&cinfo);
@@ -80,33 +100,75 @@ int read_jpeg(const char *filename)
 }
 
 int
-main ()
+main (int argc, char *argv[])
 {
-    int32_t histogram1[256];
-    int32_t histogram2[256];
-    read_jpeg("test1.jpg");
-    memcpy(histogram1, histogram, sizeof(histogram));
-    memset(histogram, 0, sizeof(histogram));
-    read_jpeg("test2.jpg");
-    memcpy(histogram2, histogram, sizeof(histogram));
+    int32_t histogram1[NUM_SAMPLE][3][256];
+    int32_t histogram2[NUM_SAMPLE][3][256];
+    read_jpeg(argv[1]);
+    memcpy(histogram1, histogram, sizeof(int32_t) * NUM_SAMPLE * 3 * 256);
+    memset(histogram, 0, sizeof(int32_t) * NUM_SAMPLE * 3 * 256);
+    read_jpeg(argv[2]);
+    memcpy(histogram2, histogram, sizeof(int32_t) * NUM_SAMPLE * 3 * 256);
 
-    int sum1 = 0;
-    int sum2 = 0;
-    for (int i = 0; i < 256; i++)
+    int sum1[NUM_SAMPLE][3] = {0, };
+    int sum2[NUM_SAMPLE][3] = {0, };
+
+    for (int i = 0; i < NUM_SAMPLE; i++)
     {
-        sum1 += histogram1[i];
-        sum2 += histogram2[i];
+        for (int k = 0; k < 256; k++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                sum1[i][c] += histogram1[i][c][k];
+                sum2[i][c] += histogram1[i][c][k];
+            }
+        }
     }
 
-    float result = 0;
-    for (int i = 0; i < 256; i++)
+    float result[NUM_SAMPLE][3] = {0, };
+    float result_sum[NUM_SAMPLE];
+    for (int i = 0; i < NUM_SAMPLE; i++)
     {
-        printf("%7u %7u %lf%%\n",
-                histogram1[i], histogram2[i],
-                (float)(histogram1[i] - histogram2[i]) / (float)(sum1 + sum2) * 100);
-        result += fabsf((float)histogram1[i] - histogram2[i]) / (float)(sum1 + sum2) * 100;
+        printf("SAMEPLE %d\n", i);
+        for (int k = 0; k < 256; k++)
+        {
+#if 0
+            printf("(%u,%u,%u) (%u,%u,%u) (%lf,%lf,%lf)\n",
+                    histogram1[i][0][k],
+                    histogram1[i][1][k],
+                    histogram1[i][2][k],
+                    histogram2[i][0][k],
+                    histogram2[i][1][k],
+                    histogram2[i][2][k],
+                    (float)(histogram1[i][0][k] - histogram2[i][0][k]) / (float)(sum1[i][0] + sum2[i][0]) * 100,
+                    (float)(histogram1[i][1][k] - histogram2[i][1][k]) / (float)(sum1[i][1] + sum2[i][1]) * 100,
+                    (float)(histogram1[i][2][k] - histogram2[i][2][k]) / (float)(sum1[i][2] + sum2[i][2]) * 100);
+#endif
+
+            for (int c = 0; c < 3; c++)
+            {
+                result[i][c] +=
+                    (fabsf((float)histogram1[i][c][k] - histogram2[i][c][k])
+                    / (float)(sum1[i][c] + sum2[i][c])) * 100 / 3;
+            }
+        }
+
+        printf("result = ");
+        for (int c = 0; c < 3; c++)
+        {
+            printf("%lf ", result[i][c]);
+            result_sum[i] += result[i][c] / 3;
+        }
+        printf("\n");
+        printf("sample result = %lf\n", result_sum[i]);
     }
-    printf("%lf\n", result);
+
+    float total_result = 0;
+    for (int i = 0; i < NUM_SAMPLE; i++)
+    {
+        total_result += result_sum[i] / 10;
+    }
+    printf("\n total_result = %lf\n", total_result);
 
     exit(0);
 }
